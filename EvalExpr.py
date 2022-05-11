76a2489a04e0324bc8e47434fd683951cc5aad26
ao <abdullahosama15@gmail.com>
2022-05-11 10:59:35 +0200

    Changing type of error

--- EvalExpr.py
+++ EvalExpr.py
@@ -1,3 +1,4 @@
+from tokenize import Name
 import numpy as np
 
 ALLOWED_CHARS = ['+', '-', '/', '*', '^', ' ', '(', ')', \
@@ -7,15 +8,17 @@ def evaluateExpression(expr, lowerBound, upperBound, variableName = 'x', step=No
     """
         Evaluating mathematical expression for the range of values between lowerBound to upperBound (inclusive)
     """
-
     # Validate input values
     if lowerBound >= upperBound:
         raise ValueError("lowerBound: {} must be greater than upperBound: {}".format(lowerBound, upperBound))
-    if not expr:
-        raise ValueError('Expression can not be left empty')
+    
     for char in expr:
-        if not (char >= 'a' and char <= 'z') and char not in ALLOWED_CHARS:
+        if  (char >= 'a' and char <= 'z') and char != variableName:
+            raise NameError('Only \'{}\' is accepted as a variable name'.format(variableName))
+        elif char != variableName and char not in ALLOWED_CHARS:
+            print(char)
             raise SyntaxError('invalid expression')
+
     if '**' in expr or '//' in expr:
         raise SyntaxError('invalid expression')
 
@@ -23,7 +26,7 @@ def evaluateExpression(expr, lowerBound, upperBound, variableName = 'x', step=No
 
     if step is None:
         step = min(1 / 100, 1 / 100 * (upperBound - lowerBound))   
-        
+
     outputValues = np.asarray(1)
     inputValues = np.arange(lowerBound, upperBound + step, step=step)
     noOfPoints = inputValues.size
